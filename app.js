ea41de555fb6716081a498ee852e505ac1d4a7af
Shah, Rickin <rickin.shah@pearson.com>
2014-08-19 17:17:02 -0400

    Began work on integrating requireJS into the server-side code and developing a router module capable of directing web requests to the proper controller

--- app.js
+++ app.js
@@ -2,19 +2,52 @@
 {
 	'use strict';
 
+// ----------------- EXTERNAL MODULES --------------------------
 	var _http_ = require('http'),
-		_url_ = require('url');
+		_url_ = require('url'),
+		__requireJS__ = require('requirejs');
+
+// ----------------- ENUM/CONSTANTS --------------------------
+	var HOME_CONTROLLER = 'home',
+		INIT_ACTION = 'init';
+
+	// Configure requireJS before launching the server or doing anything else
+	__requireJS__.config(
+	{
+		baseUrl: 'server',
+		nodeRequire: require
+	});
 
 	// Define a server that will act as a gateway point for all incoming server requests
 	_http_.createServer(function(request, response)
 	{
-		console.log('Initiated request to server!');
-		console.log(_url_.parse(request.url, true));
+		// From here on out, operate within the boundaries of requireJS
+		__requireJS__(['config/router'], function(router)
+		{
+			var urlObj = _url_.parse(request.url.trim(), true),
+				routeSigns = urlObj.pathname.split('/'),
+				controllerName = HOME_CONTROLLER, // default controller
+				actionName = INIT_ACTION,
+				params = urlObj.query;
 
-		response.writeHead(200);
-		response.end('Yo Earth');
+			// If a path has not been defined, the server will route the request to the home page, by default
+			// If a path has been defined however, the server will route the request to the controller indicated within the URL
+			if (urlObj.pathname !== '/')
+			{
+				controllerName = routeSigns[1];
+				actionName = routeSigns[2] || INIT_ACTION;
+			}
 
+/*
+			__requireJS__([router.findRoute(controllerName)], function(controller)
+			{
+				controller[actionName](params);
+			});
+*/
+			response.writeHead(200);
+			response.end((router.findRoute(controllerName)));
+		});
 	}).listen(3000);
 
-	console.log('Server started');
+	console.log('Server started!');
 }());
---
+++ server/config/router.js
@@ -0,0 +1,39 @@
+/**
+ * @module router
+ */
+
+define([], function()
+{
+	'use strict';
+
+// ----------------- ENUM/CONSTANTS --------------------------
+	var CONTROLLERS_DIRECTORY = 'controller/',
+		PAGE_NOT_FOUND_CONTROLLER = '404';
+
+// ----------------- ROUTES --------------------------
+	var ROUTES =
+		{
+			'suicidePools' : 'pools',
+
+
+			'404' : '404'
+		};
+
+// ----------------- MODULE DEFINITION --------------------------
+	return {
+
+		/**
+		 * Function responsible for deducing a filepath (from the server folder) to the controller whose name is passed
+		 *
+		 * @param {String} controller - the name of the controller for which a relative path will be calculated
+		 *
+		 * @returns {String} - a relative filepath to the controller that can be consumed by requireJS to fetch files
+		 */
+		findRoute: function(controller)
+		{
+			return (ROUTES[controller] ? CONTROLLERS_DIRECTORY + ROUTES[controller] :
+				CONTROLLERS_DIRECTORY + ROUTES[PAGE_NOT_FOUND_CONTROLLER]);
+		}
+
+	};
+});
