8ff1529c6dd7e21a3edbd2a6c73e8baeafef0327
kinsho <kinsho@gmail.com>
2014-08-23 21:24:19 -0400

    Made some fantastic progress on standardizing the way templates are compiled in the server code base

--- server/utility/fileManager.js
+++ server/utility/fileManager.js
@@ -8,12 +8,16 @@ define(['app-root-path', 'path', 'fs', 'co', 'thunkify'], function(appPath, path
 		LIBRARY_DIRECTORY = 'library/',
 		FOUNDATION_DIRECTORY = 'foundation/',
 		STYLESHEET_DIRECTORY = 'styles/css/',
-		VIEWS_DIRECTORY = 'views/';
+		MODULES_DIRECTORY = 'modules/',
+		VIEWS_DIRECTORY = 'views/',
+
+		TEMPLATE_EXTENSION = '.handlebars';
 
 // ----------------- ASYNCHRONOUS THUNKS --------------------------
 
 	var fsReadDir = thunkify(fs.readdir),
-		fsStat = thunkify(fs.stat);
+		fsStat = thunkify(fs.stat),
+		fsReadFile = thunkify(fs.readFile);
 
 // ----------------- PRIVATE FUNCTIONS --------------------------
 
@@ -33,7 +37,7 @@ define(['app-root-path', 'path', 'fs', 'co', 'thunkify'], function(appPath, path
 		 *
 		 * @author kinsho
 		 */
-	var fileScraper = co(function* (directoryName, fileName, recursiveRead)
+	var fileNameScraper = co(function* (directoryName, fileName, recursiveRead)
 		{
 			var fileNames,
 				fileStats,
@@ -71,7 +75,7 @@ define(['app-root-path', 'path', 'fs', 'co', 'thunkify'], function(appPath, path
 						{
 							// Recursively scrape out files within the subdirectory provided that the recursiveRead
 							// flag was set
-							fileScraper(fileNames[i], null, true);
+							fileNameScraper(fileNames[i], null, true);
 						}
 					}
 
@@ -82,22 +86,103 @@ define(['app-root-path', 'path', 'fs', 'co', 'thunkify'], function(appPath, path
 					console.error(error);
 				}
 			}
+		}),
+
+		/**
+		 * Generator function responsible for reading the contents of files given the system path to that file
+		 *
+		 * @param {Array[String] | String} filePaths - either an array containing multiple system file paths or a
+		 * 		string representing just one system file path
+		 *
+		 * @returns {Array[String] | String} - if given a collection of multiple system file paths, the function will
+		 * 		return a collection of strings containing the contents of each of those files indicated by the file
+		 * 		paths. If given a string representing just one system file path, function will return a simple string
+		 * 		representing the contents of that one targeted file
+		 *
+		 * @author kinsho
+		 */
+		fileContentScraper = co(function* (filePaths)
+		{
+			var fileContents;
+
+			if (filePaths instanceof Array)
+			{
+				fileContents = yield filePaths.map(function(fileName)
+				{
+					return fsReadFile(fileName);
+				});
+			}
+			else
+			{
+				fileContents = yield fsReadFile(filePaths);
+			}
+
+			return fileContents;
 		});
 
 // ----------------- MODULE DEFINITION --------------------------
 	var my =
 	{
-		fetchAllFoundationViews: function()
+		/**
+		 * Generator function that returns all foundational view file names
+		 *
+		 * @returns {Array[String]} - a collection of file names
+		 *
+		 * @author kinsho
+		 */
+		fetchAllFoundationViews: co(function* ()
 		{
-			return fileScraper(CLIENTS_DIRECTORY + VIEWS_DIRECTORY + FOUNDATION_DIRECTORY, null, true);
-		},
+			return yield fileNameScraper(CLIENTS_DIRECTORY + VIEWS_DIRECTORY + FOUNDATION_DIRECTORY, null, true);
+		}),
 
-		fetchAllLibraryStylesheets: function()
+		/**
+		 * Generator function that returns all library and foundational stylesheet file names
+		 *
+		 * @returns {Array[String]} - a collection of file names
+		 *
+		 * @author kinsho
+		 */
+		fetchAllLibraryStylesheets: co(function* ()
 		{
-			return fileScraper(CLIENTS_DIRECTORY + STYLESHEET_DIRECTORY + LIBRARY_DIRECTORY, null, true);
-		}
+			return yield (fileNameScraper(CLIENTS_DIRECTORY + STYLESHEET_DIRECTORY + LIBRARY_DIRECTORY, null, true).
+				concat(fileNameScraper(CLIENTS_DIRECTORY + STYLESHEET_DIRECTORY + MODULES_DIRECTORY, null, true)));
+		}),
+
+		/**
+		 * Generator function that returns the content a specific template file
+		 *
+		 * @param {String} viewFolder - the name of the sub-directory within the views folder where
+		 * 		the targeted template resides
+		 * @param {String} templateName - the name of the template to fetch
+		 *
+		 * @returns {String} - the contents of the template specified by the parameters
+		 *
+		 * @author kinsho
+		 */
+		fetchTemplate: co(function* (viewFolder, templateName)
+		{
+			return yield fileContentScraper(CLIENTS_DIRECTORY + VIEWS_DIRECTORY + viewFolder +
+				templateName + TEMPLATE_EXTENSION);
+		}),
+
+		/**
+		 * Generator function that returns the content a specific template file
+		 *
+		 * @param {String} viewFolder - the name of the sub-directory within the views folder where
+		 * 		the targeted template resides
+		 * @param {String} templateName - the name of the template to fetch
+		 *
+		 * @returns {String} - the contents of the template specified by the parameters
+		 *
+		 * @author kinsho
+		 */
+		fetchTemplates: co(function* (viewFolder)
+		{
+			// Fetch all the file names from the passed view sub-directory
+			var filePaths = yield (fileNameScraper(viewFolder));
+			return yield fileContentScraper(filePaths);
+		})
 	};
-
 
 // ----------------- END --------------------------
 	return my;
--- server/utility/handlebarsManager.js
+++ server/utility/handlebarsManager.js
@@ -1,4 +1,4 @@
-define(['Handlebars'], function(handlebars)
+define(['Handlebars', 'path', 'co', 'utility/fileManager'], function(Handlebars, path, co, fileManager)
 {
 	'use strict';
 
@@ -11,15 +11,40 @@ define(['Handlebars'], function(handlebars)
 		 * to the template being compiled.
 		 *
 		 * @param {JSON} data - the data to roll into the template
-		 * @param {String} templatePath - the location of the template relative to the project root
+		 * @param {String} templateDirectory - the directory in which the template is located
+		 * @param {String} templateName - the name of the template
 		 * @param {Array[Function]} [helpers] - an associative array of helper functions to register into handlebars
 		 * 		prior to the compilation of the template
 		 *
 		 * @author kinsho
 		 */
-		populateTemplate: function(data, templatePath, helpers)
+		populateTemplate: co(function* (data, templateDirectory, templateName, helpers)
 		{
-			helpers = helpers || [];
-		}
+			var keys,
+				template,
+				i;
+
+			helpers = helpers || {};
+			keys = Object.keys(helpers);
+
+			// Register any helpers that may have been passed into the function
+			for (i = keys.length - 1; i >= 0; i -= 1)
+			{
+				Handlebars.registerHelper(keys[i], helpers[i]);
+			}
+
+			// Check to see if the template has already been precompiled. If not, fetch the template, comiple it,
+			// cache it.
+			template = Handlebars.templates[templateDirectory];
+			if (!template)
+			{
+				template = yield fileManager.fetchTemplate(templateDirectory, templateName);
+				template = Handlebars.compile(template);
+				Handlebars.template[templateDirectory] = template;
+			}
+
+			// Feed the data into the template and return the resulting HTML
+			return template(data);
+		})
 	};
 });
